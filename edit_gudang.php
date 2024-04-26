dfd344f06651e6b109c6f36e65b32c0a0043d97a
Aku-Mars <marifinsyam73@gmail.com>
2024-04-26 16:10:47 +0700

    [~]Percobaan

--- edit_gudang.php
+++ edit_gudang.php
@@ -1,6 +1,30 @@
 <?php
 session_start();
 
+// Data dummy gudang (contoh)
+$gudangData = array(
+    1 => array(
+        'penyewa' => 'Penyewa A',
+        'tanggal_sewa' => '2024-04-27',
+        'tanggal_akhir_sewa' => '2024-05-27',
+        'lokasi' => 'Lokasi A',
+        'barang' => array(
+            array('nama' => 'Barang 1', 'id' => '1', 'jumlah' => 5),
+            array('nama' => 'Barang 2', 'id' => '2', 'jumlah' => 10)
+        )
+    ),
+    2 => array(
+        'penyewa' => 'Penyewa B',
+        'tanggal_sewa' => '2024-04-28',
+        'tanggal_akhir_sewa' => '2024-05-28',
+        'lokasi' => 'Lokasi B',
+        'barang' => array(
+            array('nama' => 'Barang 3', 'id' => '3', 'jumlah' => 8),
+            array('nama' => 'Barang 4', 'id' => '4', 'jumlah' => 12)
+        )
+    )
+);
+
 // Periksa apakah pengguna sudah login sebagai operator
 if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'operator') {
     header('Location: index.php');
